8343bc8d45e4e93cf1f62dc0c2ca21e3706a82d0
Pawan Theekshana <86646086+ImTheekshannBro@users.noreply.github.com>
2021-07-10 03:39:54 -0700

    Update __main__.py

--- DaisyXMusic/__main__.py
+++ DaisyXMusic/__main__.py
@@ -1,4 +1,4 @@
-# DofiniMusic (Telegram bot project)
+# Mizu Music (Telegram bot project)
 # Copyright (C) 2021  Pawan Theekshana & Rojserbest
 
 # This program is free software: you can redistribute it and/or modify
